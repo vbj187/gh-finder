a5305747e49cb22c46301940e7a296eba1feac3d
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-13 11:26:08 +0530

    alert set for empty search

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import NavBar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // async componentDidMount() {
@@ -34,17 +36,30 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  setAlert = (message, type) => {
+    this.setState({
+      alert: { message, type },
+    });
+
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='App'>
         <NavBar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,6 +10,7 @@ export class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onChange = (event) => {
@@ -19,11 +20,16 @@ export class Search extends Component {
   //search github users
   onSubmit = (event) => {
     event.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit}>
@@ -40,11 +46,8 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
-        {this.props.showClear && (
-          <button
-            className='btn btn-light btn-block'
-            onClick={this.props.clearUsers}
-          >
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
             Clear
           </button>
         )}
